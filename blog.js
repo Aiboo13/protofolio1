166835850bbebfac53a3ad84e2111a088e7b6d58
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-02-10 22:16:13 +0700

    clear

--- blog.js
+++ blog.js
@@ -1,1 +1,13 @@
 document.querySelector('main').classList.add('background')
+document.querySelectorAll('a').classList.add('.a')
+
+AOS:init();
+
+document.querySelectorAll(".nav-link").forEach(item => {
+  item.addEventListener("click", () => {
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+    if (navbarCollapse.classList.contains("show")) {
+      new bootstrap.Collapse(navbarCollapse).hide();
+    }
+  });
+});
