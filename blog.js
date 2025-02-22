3108e984f51f34c7fbde512216fbc37ecd2b8794
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-02-23 05:53:56 +0700

    complite

--- blog.js
+++ blog.js
@@ -1,17 +1,11 @@
-document.querySelector('main').classList.add('background')
-document.querySelectorAll('a').classList.add('.a')
+document.querySelector('main').classList.add('background');
+document.querySelectorAll('a').forEach(a => a.classList.add('a'));
 
-AOS:init();
-
-
-document.querySelectorAll(".nav-link").forEach(item => {
-  item.addEventListener("click", () => {
-    const navbarCollapse = document.querySelector(".navbar-collapse");
-    if (navbarCollapse.classList.contains("show")) {
-      new bootstrap.Collapse(navbarCollapse).hide();
-    }
-  });
+AOS.init({
+  duration: 1000, // Animation duration
+  delay: 200, // Delay in milliseconds
 });
+
 document.querySelectorAll(".nav-link").forEach(item => {
   item.addEventListener("click", () => {
     const navbarCollapse = document.querySelector(".navbar-collapse");
